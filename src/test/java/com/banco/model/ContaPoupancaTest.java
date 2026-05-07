0cc30c0c5633ad1d11826b6c9a10a9fda4f5256f
tomzudo <araujoerivelton324@gmail.com>
2026-05-06 22:54:13 -0300

    test: testes de dados inválidos: null, string vazia e saldo negativo no metodo de  criacao de conta na classe ContaPoupanca aprovado

--- src/test/java/com/banco/model/ContaPoupancaTest.java
+++ src/test/java/com/banco/model/ContaPoupancaTest.java
@@ -1,5 +1,21 @@
 package com.banco.model;
 
-public class ContaPoupancaTest {
-    
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+    class ContaPoupancaTest {
+    @Test
+    void deveImpedirDadosInválidos(){
+        assertAll(
+
+            () -> assertThrows(IllegalArgumentException.class, 
+                () -> new ContaPoupanca("", 100)),
+            
+            () -> assertThrows(IllegalArgumentException.class, 
+                () -> new ContaPoupanca("Tom", -100)),
+
+            () -> assertThrows(IllegalArgumentException.class, 
+                () -> new ContaPoupanca(null, 100))
+            );
+    }
 }
