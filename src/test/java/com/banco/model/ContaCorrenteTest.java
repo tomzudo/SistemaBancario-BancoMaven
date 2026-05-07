8c623cea78937adf6afcf2551036726cb8c61a60
tomzudo <araujoerivelton324@gmail.com>
2026-05-06 21:41:37 -0300

    chore: teste de dados invalidos [null, espaço em branco  e saldo negativo] na ContaCorrente aprovado

--- src/test/java/com/banco/model/ContaCorrenteTest.java
+++ src/test/java/com/banco/model/ContaCorrenteTest.java
@@ -1,5 +1,23 @@
 package com.banco.model;
 
-public class ContaCorrenteTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaCorrenteTest {
     
+    @Test
+    void deveImpedirDadosInválidos(){
+        assertAll(
+
+            () -> assertThrows(IllegalArgumentException.class, 
+                () -> new ContaCorrente("", 100)),
+            
+            () -> assertThrows(IllegalArgumentException.class, 
+                () -> new ContaCorrente("Tom", -100)),
+
+            () -> assertThrows(IllegalArgumentException.class, 
+                () -> new ContaCorrente(null, 100))
+            );
+    }
+
 }
